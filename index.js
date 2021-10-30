e26119e24c98781f8f24c43e1e2a2dc3a593dfcc
Kumar Saurabh <kumar.saurabh@vernost.in>
2021-10-31 00:35:23 +0530

    commit with basic details

---
+++ index.js
@@ -0,0 +1,16 @@
+#!/usr/bin/env node
+
+console.clear();
+
+console.log(`
+Kumar Saurabh 
+
+Senior Software Engineer having more than 4+ years Experience
+with Javascript and related technologine like Node.js and various 
+frontend framework like react, Ext.js, Jquery.
+Always willing to learn new things blockchain entusiast currently 
+pursuing master from LJMU UK.
+
+:book: Github: https://github.com/zeay
+
+`);
